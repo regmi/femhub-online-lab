61c9a2eccf29fa366d187c1dade61a351ff74626
Sameer Regmi <regmisk@gmail.com>
2010-12-03 18:17:42 -0800

    Change mesheditor window and html tags to fix fluttering

--- ui/js/femhub/femhub.mesheditor.js
+++ ui/js/femhub/femhub.mesheditor.js
@@ -6,15 +6,15 @@ FEMhub.MeshEditor = Ext.extend(FEMhub.Window, {
         Ext.apply(config, {
             title: "Mesh Editor",
             layout: 'fit',
-            width: 885,
-            height: 595,
+            width: 870,
+            height: 610,
             iconCls: 'femhub-mesheditor-icon',
             bodyCssClass: 'femhub-mesheditor-body',
             closable: true,
             onEsc: Ext.emptyFn,
             items: [{
                     "title": "Beta Version",
-                    "html": '<object type="application/x-shockwave-flash" data="/static/external/MeshEditor.swf" width="100%" height="100%"><param name="flashvars" value="output_cell=0&nodes=&elements=& boundaries=&curves=&var_name=m" /><p>Alternative ContentXX</p></object>',
+                    "html": '<table height="100%" width="100%" border="0" bgcolor="#728893"><tr><td valign="middle" align="center"><object type="application/x-shockwave-flash" data="/static/external/MeshEditor.swf" width="850" height="525"><param name="flashvars" value="output_cell=0&nodes=&elements=& boundaries=&curves=&var_name=m" /><p>Alternative ContentXX</p></object></tr></td></table>',
                     flex: 1,
                 }],
             buttons: [{
@@ -37,4 +37,3 @@ FEMhub.Modules.MeshEditor = Ext.extend(FEMhub.Module, {
     },
     winCls: FEMhub.MeshEditor,
 });
-
